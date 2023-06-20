46bf8d09ccb5350f6e2e114a87304e321ff5eddb
MahidharNagolu <112749213+MahidharNagolu@users.noreply.github.com>
2023-06-20 10:14:14 +0530

    Create Day33.R

---
+++ Day33.R
@@ -0,0 +1,7 @@
+Row Percentages
+
+The R package “tigerstats” is required for the next two exercises.
+
+1) Create an xtabs() formula that cross-tabulates “Status“, and “Test“.
+
+2) Enclose the xtabs
