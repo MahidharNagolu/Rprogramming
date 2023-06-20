88045e99a585352cef55224c76c2181dcfc68dbf
MahidharNagolu <112749213+MahidharNagolu@users.noreply.github.com>
2023-06-20 10:11:02 +0530

    Create Day 4.3 .R

---
+++ Day 4.3 .R
@@ -0,0 +1,13 @@
+data(delivery)
+
+str(delivery)
+
+model <- lm(delTime ~ n.prod + distance, data = delivery)
+
+summary(model)
+
+new_data <- data.frame(n.prod = 9, distance = 450)
+
+prediction <- predict(model, newdata = new_data)
+
+prediction
